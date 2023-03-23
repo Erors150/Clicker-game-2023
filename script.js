4a6f2c3be7801ab6d1e410e479c4216a01a3cab5
Erors150 <128698438+Erors150@users.noreply.github.com>
2023-03-23 13:22:40 +0200

    files imporded

--- script.js
+++ script.js
@@ -4,29 +4,38 @@ let red = 0;
 let green = 0;
 let blue = 0;
 
-let diametrs = 200;
+let diameter = 200;
+
+let clickerButton = document.getElementById("button");
+let scoreDisplay = document.getElementById("score");
 
 function OnButtonClick(){
     punkti += 1;
-    document.getElementById("score").innerHTML = punkti;
-
+    scoreDisplay.innerHTML = punkti;
+    
     red = getRandomInt(256);
     green = getRandomInt(256);
     blue = getRandomInt(256);
 
     let rgbCode = "rgb(" + red.toString() + "," + green.toString() + "," + blue.toString() + ")";
-    document.getElementById("button").style.backgroundColor = rgbCode;
+    clickerButton.style.backgroundColor = rgbCode;
 
-diametrs -= 5;
+    diameter -= 5;
 
-if (diametrs <= 5){
-document.getElementById("score").innerHTML = 'You Win'
-}
-document.getElementById("button").style.width = diametrs.toString() + "px";
-document.getElementById("button").style.height = diametrs.toString() + "px";
+    if(diameter <= 5){
+        scoreDisplay.innerHTML = "You Won!"
+    }
+
+    clickerButton.style.width = diameter.toString() + "px";
+    clickerButton.style.height = diameter.toString() + "px";
 
 }
 
 function getRandomInt(max){
     return Math.floor(Math.random() * max);
 }
+
+// UZDEVUMS 1
+// Kad klikšķina uz pogu
+// lai poga maina savu krāsu nejaušā veidā
+// lai ir random krāsa
